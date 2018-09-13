0cf527e8042beca0129769a6fb795288a7042951
Ray Millares <rmill013@gmail.com>
2018-09-13 16:08:09 -0400

    FactoryObjects now becomes parented to FactoryContainer when placed inside. Adds basic Presser Functionality

--- Assets/Code/Prototype/FactoryObject.cs
+++ Assets/Code/Prototype/FactoryObject.cs
@@ -29,7 +29,7 @@ public class FactoryObject : MonoBehaviour, IConveyerMover
     //    Initialize();
     //}
 
-    private void Start()
+    private void Awake()
     {
         m_interaction = GetComponent<Hi5_Glove_Interaction_Item>();
         m_rigid = GetComponent<Rigidbody>();
@@ -40,8 +40,7 @@ public class FactoryObject : MonoBehaviour, IConveyerMover
     private void Update()
     {
         CheckState();
-        if (b_isFalling)
-            CheckForGround();
+        
 
         if (Input.GetKeyDown(KeyCode.R))
             ReadyObject();
@@ -60,6 +59,12 @@ public class FactoryObject : MonoBehaviour, IConveyerMover
             //Vector3 translationVector = new Vector3(m_conveyerSpeed.Value, 0, 0);
             //transform.Translate(translationVector * Time.deltaTime, Space.World);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (b_isFalling)
+            CheckForGround(transform.position);
     }
 
     private void OnDisable()
@@ -94,8 +99,8 @@ public class FactoryObject : MonoBehaviour, IConveyerMover
     public void Initialize()
     {
         gameObject.name = "FactoryObjectPrefab";
-        
 
+        m_interaction.enabled = false;
         b_isBoxed = false;
         b_isSealed = false;
         b_isStamped = false;
@@ -114,19 +119,27 @@ public class FactoryObject : MonoBehaviour, IConveyerMover
 
     private void OnTriggerEnter(Collider other)
     {
-        // THIS MAY NOT BE NECESSARY
-        // We want to be able to drop FactoryObjs onto Default Layer GameObjects so here we handle converting a Factory Object
-        // from the HI5ObjectGrasp Layer to the Default Layer so that we can enable desired interaction with Default Layered Objects.
+        // Once we pass through the LayerConverter we are in the box. So after setting a few key parameter
+        // we then parent the FactoryObject to the container and make our RB kinematic.
+        // This ensures that the ball will not fly out of the box if moved too quickly for the physics simulation
         if (other.gameObject.tag == "LayerConverter")
         {
+            b_isBoxed = true;
+            // todo play sound?
             Debug.LogWarning("Converting");
             gameObject.layer = m_defaultLayer;
             // Disable Interaction item
             m_interaction.enabled = false;
-            m_rigid.useGravity = true;
-            m_rigid.isKinematic = false;
+            m_rigid.useGravity = false;
+            m_rigid.isKinematic = true;
             m_rigid.constraints = RigidbodyConstraints.None;
-            m_rigid.constraints = RigidbodyConstraints.FreezeAll;
+
+            UpdateCheckList();
+
+            // Parenting to container
+            transform.parent = other.transform;
+            transform.localScale = new Vector3(.05f, 0.05f, 0.05f);
+
 
         }
     }
@@ -172,14 +185,14 @@ public class FactoryObject : MonoBehaviour, IConveyerMover
             b_isFalling = false;
     }
 
-    void CheckForGround ()
+    void CheckForGround (Vector3 castPosition)
     {
         int layerMask = 1;
-        Ray ray = new Ray(transform.position, Vector3.down);
+        Ray ray = new Ray(castPosition, Vector3.down);
         RaycastHit hit;
         if (Physics.Raycast(ray.origin, ray.direction, out hit, 20f, layerMask, QueryTriggerInteraction.Collide))
         {
-            if (Mathf.Abs(hit.distance) < 0.05f)
+            if (Mathf.Abs(hit.distance) < 0.15f)
             {
                 m_interaction.mstatemanager.ChangeState(E_Object_State.EStatic);
                 m_interaction.mstatemanager.StopThrowMove();
---
+++ Assets/Code/Prototype/Presser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Presser : MonoBehaviour {
+
+    public AnimationClip pressClip;
+    private Animator m_anim;
+
+	// Use this for initialization
+	void Start () {
+
+        m_anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+            m_anim.SetTrigger("Press");
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            print("Increase animation speed");
+            m_anim.speed += 0.5f;
+        }
+	}
+}
